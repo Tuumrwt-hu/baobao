07c5a14e0096ab3aa799f2c823bc185bd0ddf2ef
Tuumrwt-hu <dupajian1991da@126.com>
2021-06-15 15:01:59 +0800

    添加了java文件

---
+++ baobao/baobao-child/src/main/java/com/baobao/App.java
@@ -0,0 +1,15 @@
+package com.baobao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World3!" );
+        System.out.println( "Hello World2!" );
+    }
+}
